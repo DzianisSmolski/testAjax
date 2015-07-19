d7ed55dc55f0655e3f667af8de8c650a19a2f586
Денис <@LorDenPc>
2015-07-19 07:55:59 -0300

    FIRST

---
+++ crosAjax/src/den/dog/by/TestRequest.java
@@ -0,0 +1,72 @@
+package den.dog.by;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestRequest
+ */
+public class TestRequest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("I init!");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("I get!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("--------------------------------------------------");
+		System.out.println("I is post!");
+		System.out.println(request.getHeader("AUTHTOKEN"));
+		response.setHeader("Access-Control-Allow-Origin", "*");
+	}
+
+	/**
+	 * @see HttpServlet#doOptions(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("I is Options!");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, AUTHTOKEN");
+		response.setHeader("Access-Control-Allow-Methods:", "POST, GET, OPTIONS");
+		Enumeration<String> stream = request.getHeaderNames();
+		while (stream.hasMoreElements()) {
+			String headerName = stream.nextElement();
+			System.out.println(headerName + ": " + request.getHeader(headerName));
+			response.getOutputStream().print(headerName + ": " + request.getHeader(headerName));
+		}
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("I Destroy!");
+	}
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1)
+			throws ServletException, IOException {
+		super.service(arg0, arg1);
+		System.out.println("I is Service!");
+	}
+
+}
